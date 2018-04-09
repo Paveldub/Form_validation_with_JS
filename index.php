784ddb995058a1a329e6fe1bf76f9e579bf43fdc
Mac <mac@MacBook-Pro-Mac.local>
2018-04-09 18:30:00 +0300

    php_file

--- index.php
+++ index.php
@@ -3,4 +3,17 @@
         $user = $_POST["username"];
         echo "Welcome ".$user;
     }
+
+    $inp = $_POST['username'];
+
+    switch($inp) {
+        case 'Garry':
+            echo "Griffindor";
+            break;
+        case 'Germiona':
+            echo 'Ко мне в кабинет';
+            break;
+        default:
+            echo 'Фу, магл';        
+    }
 ?>
