27fdf99829c29d3fb49f66228cb6a642a71b499f
Mac <mac@MacBook-Pro-Mac.local>
2018-04-10 14:15:40 +0300

    Modal_dialog

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
+// Get modal element
+let modal = document.getElementById('simpleModal');
+
+// Get open modal
+let modalBtn = document.getElementById('button');
+
+// Get close button
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+// Lister for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e) {
+  if(e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
 let button = document.getElementById("button");
 
 button.onclick = function(e) {
